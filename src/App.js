fde4cd7047a6bbb70169d25a30c76999432b03e6
1js2002 <dgoud99120@gmail.com>
2023-03-13 16:57:26 +0530

    installation of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{backgroundImage: `url(${coldBg})` }}>
+       <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="enter a city.." />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London</h3>
+              <img src="" alt="weatherlogo"></img>
+              <h3>cloudy</h3>
+            </div>
+            <div className="temper">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+          {/* bottom description */}
+        </div>
+
+       </div>
     </div>
   );
 }
